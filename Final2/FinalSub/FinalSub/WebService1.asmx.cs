631881c8c9d9a22fae2a4e80be300b7da87ab1b3
gyb2015 <gyb2015@uw.edu>
2014-02-12 18:09:30 -0800

    PA2

---
+++ Final2/FinalSub/FinalSub/WebService1.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace FinalSub
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "WebApplication1")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Allow this Web Service to be called from script, using ASP.NET AJAX
+    [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public List<string> GetSearchSuggestions(string input)
+        {
+            return Global.trie.SearchPhrasesForPrefix(input);
+        }
+    }
+}
