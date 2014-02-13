cba8053b00ced42435aa3b87d50b87a53d2fab6b
gyb2015 <gyb2015@uw.edu>
2014-02-13 00:28:04 -0800

    global again

---
+++ Final2/FinalSub/FinalSub/Global.asax.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using FinalSub;
+
+namespace WebApplication1
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public static Trie trie;
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Build a Trie when web app starts
+            trie = new Trie();
+
+            using (StreamReader sr = new StreamReader(Server.MapPath("enwiki-20131104-all-titles-in-ns0-preprocessed")))
+            {
+                while (sr.EndOfStream == false)
+                {
+                    string line = sr.ReadLine();
+
+                    try
+                    {
+                        trie.InsertPhrase(line);
+                    }
+                    catch (OutOfMemoryException)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
